e3b4fec81392401979175b5cb9ab9a4a17f7e7c6
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-09-26 16:52:12 +0100

    view-mode-supported

--- SimulationCases/runCode.sh
+++ SimulationCases/runCode.sh
@@ -19,18 +19,52 @@ if ! command -v qcc &> /dev/null; then
     exit 1
 fi
 
-FILE_NAME=${1:-arrestedCoalescence} # pass from command line, default to droplet_shear
+# Parse command-line arguments
+VIEW_MODE=false
+FILE_NAME=""
+
+for arg in "$@"; do
+    case $arg in
+        --view)
+            VIEW_MODE=true
+            ;;
+        *)
+            if [[ -z "$FILE_NAME" && "$arg" != "--view" ]]; then
+                FILE_NAME="$arg"
+            fi
+            ;;
+    esac
+done
+
+# Set default filename if not provided
+FILE_NAME=${FILE_NAME:-plateau}
 
 mkdir -p $FILE_NAME
 
-# Detect platform and set appropriate compiler flags
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-    CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 -disable-dimensions -I$PWD/src-local -I$PWD/../src-local $FILE_NAME.c -o $FILE_NAME/$FILE_NAME -lm
+# Conditional compilation based on mode
+if [ "$VIEW_MODE" = true ]; then
+    # Visualization mode - no MPI, with OpenGL libraries
+    qcc -O2 -Wall -disable-dimensions -I$PWD/src-local -I$PWD/../src-local $FILE_NAME.c -o $FILE_NAME/$FILE_NAME -L$BASILISK/gl -lglutils -lfb_tiny -lm
 else
-    # Linux
-    CC99='mpicc -std=c99 -D_GNU_SOURCE=1' qcc -Wall -O2 -D_MPI=1 -disable-dimensions -I$PWD/src-local -I$PWD/../src-local $FILE_NAME.c -o $FILE_NAME/$FILE_NAME -lm
+    # MPI mode - existing compilation
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS
+        CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 -disable-dimensions -I$PWD/src-local -I$PWD/../src-local $FILE_NAME.c -o $FILE_NAME/$FILE_NAME -lm
+    else
+        # Linux
+        CC99='mpicc -std=c99 -D_GNU_SOURCE=1' qcc -Wall -O2 -D_MPI=1 -disable-dimensions -I$PWD/src-local -I$PWD/../src-local $FILE_NAME.c -o $FILE_NAME/$FILE_NAME -lm
+    fi
 fi
+
 cd $FILE_NAME
-mpirun -np 4 ./$FILE_NAME
+
+# Conditional execution based on mode
+if [ "$VIEW_MODE" = true ]; then
+    # Direct execution for visualization
+    ./$FILE_NAME
+else
+    # MPI execution
+    mpirun -np 4 ./$FILE_NAME
+fi
+
 cd ..
